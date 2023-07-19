a749ab54af62e90f9f3da5a4453d9c9d49822154
Sani Rogers <sanirogers138@gmail.com>
2023-07-19 12:47:04 -0400

    create landing

---
+++ client/src/components/landing/LandingPage.jsx
@@ -0,0 +1,16 @@
+export const LandingPage = () => {
+
+    return(
+        <div className="landing-container">
+            <div className="landing-top">
+                <span className="name">Sani Rogers</span>
+                <span className="title">Full-Stack Developer</span>
+            </div>
+            <div className="landing-bottom">
+                <span className="fact-first">Sounds cliché but software development is more than my career, it's a hobby.</span>
+                <span className="fact-second">I also have quite the eclectic taste in video games.</span>
+            </div>
+            <button>Dive In.</button>
+        </div>
+    )
+}
